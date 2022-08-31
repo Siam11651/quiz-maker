51c31b982435fb931ee46dd645d00d0562cbc4f6
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-31 06:07:31 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let question = document.getElementsByTagName('textarea').namedItem('question');
+let optionA = document.getElementsByTagName('input').namedItem('a');
+let optionB = document.getElementsByTagName('input').namedItem('b');
+let optionC = document.getElementsByTagName('input').namedItem('c');
+let optionD = document.getElementsByTagName('input').namedItem('d');
+let generateButton = document.getElementsByTagName('button').namedItem('generate');
+let json = 
+{
+    question: null,
+    options:
+    {
+        a: null,
+        b: null,
+        c: null,
+        d: null
+    }
+};
+
+let jsonOut = document.getElementsByTagName('textarea').namedItem('json');
+
+generateButton.onclick = function()
+{
+    json.question = question.value;
+    json.options.a = optionA.value;
+    json.options.b = optionB.value;
+    json.options.c = optionC.value;
+    json.options.d = optionD.value;
+
+    jsonOut.value = JSON.stringify(json);
+}
